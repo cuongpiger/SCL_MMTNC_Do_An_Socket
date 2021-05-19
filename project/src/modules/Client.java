8a2f683ac539a1e6ddf5f2518dfaa6e025a58189
Manh Cuong <cuongpigerr@gmail.com>
2021-05-19 17:08:32 +0700

    .

--- project/src/modules/Client.java
+++ project/src/modules/Client.java
@@ -74,13 +74,12 @@ class ClientController implements Runnable {
         int current_state = 0;
         connectFileServer();
         while (iSocket != null) {
-            iBuffer = prepareOrder();
+            byte[] buffer = prepareOrder();
 
             if (iBuffer != null && iFileServer != null) {
                 try {
-                    DatagramPacket box = new DatagramPacket(iBuffer, iBuffer.length, iFileServer, iFileServerHost.getiPort());
+                    DatagramPacket box = new DatagramPacket(buffer, buffer.length, iFileServer, iFileServerHost.getiPort());
                     iSocket.send(box);
-                    iBuffer = null;
                     current_state = 1;
                 } catch (IOException err) {
                     err.printStackTrace();
